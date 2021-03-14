cf36cf8145d41a3ffd06c0ee8bdfc207166791c9
nicolasdanet <nicolas.danet@free.fr>
2021-03-14 09:05:35 +0100

    ...

--- Spaghettis/Source/Main.cpp
+++ Spaghettis/Source/Main.cpp
@@ -23,42 +23,25 @@ class SpaghettisApplication : public juce::JUCEApplication {
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-    class MainWindow    : public juce::DocumentWindow
-    {
+    class MainWindow : public juce::DocumentWindow {
+    
     public:
-        MainWindow (juce::String name)
-            : DocumentWindow (name,
-                              juce::Desktop::getInstance().getDefaultLookAndFeel()
-                                                          .findColour (juce::ResizableWindow::backgroundColourId),
-                              DocumentWindow::allButtons)
+        MainWindow (juce::String name) : DocumentWindow (name,
+            juce::Desktop::getInstance().getDefaultLookAndFeel()
+                                        .findColour (juce::ResizableWindow::backgroundColourId),
+            DocumentWindow::allButtons)
         {
             setUsingNativeTitleBar (true);
             setContentOwned (new MainComponent(), true);
-
-           #if JUCE_IOS || JUCE_ANDROID
-            setFullScreen (true);
-           #else
             setResizable (true, true);
             centreWithSize (getWidth(), getHeight());
-           #endif
-
             setVisible (true);
         }
 
         void closeButtonPressed() override
         {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
             JUCEApplication::getInstance()->systemRequestedQuit();
         }
-
-        /* Note: Be careful if you override any DocumentWindow methods - the base
-           class uses a lot of them, so by overriding you might break its functionality.
-           It's best to do all your work in your content component instead, but if
-           you really have to override any DocumentWindow methods, make sure your
-           subclass also calls the superclass's method.
-        */
 
     private:
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
@@ -120,7 +103,7 @@ public:
     }
 
 private:
-    std::unique_ptr<MainWindow> mainWindow;
+    std::unique_ptr < MainWindow > mainWindow;
     
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
--- Spaghettis/Source/MainComponent.cpp
+++ Spaghettis/Source/MainComponent.cpp
@@ -8,7 +8,10 @@
 
 #include "MainComponent.hpp"
 
-//==============================================================================
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
 MainComponent::MainComponent()
 {
     setSize (600, 400);
@@ -16,14 +19,16 @@ MainComponent::MainComponent()
 
 MainComponent::~MainComponent()
 {
+
 }
 
-//==============================================================================
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
 void MainComponent::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
     g.setFont (juce::Font (16.0f));
     g.setColour (juce::Colours::white);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
@@ -31,7 +36,8 @@ void MainComponent::paint (juce::Graphics& g)
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+
 }
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
