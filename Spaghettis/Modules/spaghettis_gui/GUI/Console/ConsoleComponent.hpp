705fd9452f6a38132cb16fde8848afaf5eea14da
nicolasdanet <nicolas.danet@free.fr>
2021-04-05 08:49:11 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Console/ConsoleComponent.hpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Console/ConsoleComponent.hpp
@@ -19,11 +19,9 @@ class ConsoleComponent : public juce::Component, public spaghettis::Logger {
 // MARK: -
 
 public:
-    ConsoleComponent()
+    ConsoleComponent() : lines_ (0)
     {
         const auto background = juce::TextEditor::backgroundColourId;
-        
-        addAndMakeVisible (text_);
         
         text_.setMultiLine (true);
         text_.setReturnKeyStartsNewLine (true);
@@ -32,9 +30,11 @@ public:
         text_.setCaretVisible (false);
         text_.setPopupMenuEnabled (false);
         text_.setFont (Spaghettis()->getConsoleFont());
-        setSize (600, 400);
-
+        text_.setInputRestrictions (maximumLengthOfLine_);
         text_.setColour (background, Spaghettis()->getColour (Colours::consoleBackground));
+        
+        addAndMakeVisible (text_);
+        setSize (600, 400);
 
         Spaghettis()->setLogger (this);
     }
@@ -73,9 +73,23 @@ private:
         }
     }
     
-    void limitSize()
+    void removeLinesAtStartIfRequired()
     {
-        // -- TODO: Remove first lines to limit the size of the text logged.
+        lines_++;
+        
+        if (lines_ > maximumNumberOfLines_) {
+        //
+        const int numberOfLinesToRemove = (maximumNumberOfLines_ / 2) + 1;
+        
+        text_.moveCaretToTop (false);
+        
+        for (int i = 0; i < numberOfLinesToRemove; ++i) { text_.moveCaretDown (true); }
+        
+        text_.insertTextAtCaret (juce::String());
+        
+        lines_ -= numberOfLinesToRemove;
+        //
+        }
     }
 
 // -----------------------------------------------------------------------------------------------------------
@@ -85,14 +99,20 @@ private:
 public:
     void logMessage (const juce::String& m, Type type) override
     {
-        limitSize();
-        text_.setColour (juce::TextEditor::textColourId, colourWithType (type));
         text_.moveCaretToEnd();
+        text_.setColour (juce::TextEditor::textColourId, colourWithType (type));
         text_.insertTextAtCaret (m + juce::newLine);
+        
+        removeLinesAtStartIfRequired();
     }
     
 private:
     juce::TextEditor text_;
+    int lines_;
+
+private:
+    static const int maximumLengthOfLine_   = 2048;
+    static const int maximumNumberOfLines_  = 8;
 
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ConsoleComponent)
