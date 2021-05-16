3fd1cc76d28604b4274450d4efbbc71a53f47c4a
nicolasdanet <nicolas.danet@free.fr>
2021-05-16 15:19:23 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Application/MenuModel.hpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Application/MenuModel.hpp
@@ -42,18 +42,16 @@ public:
         //
         menu.addCommandItem (commandManager_, Commands::fileOpen);
         menu.addSeparator();
-        menu.addCommandItem (commandManager_, Commands::paths);
         #if SPAGHETTIS_MENUBAR
         menu.addCommandItem (commandManager_, Commands::preferences);
         #endif
+        menu.addCommandItem (commandManager_, Commands::paths);
         menu.addSeparator();
         menu.addCommandItem (commandManager_, Commands::rescan);
         
         #if SPAGHETTIS_MENUBAR
-        #if JUCE_LINUX
         menu.addSeparator();
         menu.addCommandItem (commandManager_, juce::StandardApplicationCommandIDs::quit);
-        #endif
         #endif
         //
         } else if (n == 1) {
