3f9f4fe9bccc5d8e4705660ae3812fb2d2b214cf
nicolasdanet <nicolas.danet@free.fr>
2021-06-09 15:09:45 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/SearchPaths/SearchPathsFactory.cpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/SearchPaths/SearchPathsFactory.cpp
@@ -18,9 +18,10 @@ void SearchPathsFactory::setToolbarButton (int itemId, bool isToggle, juce::Tool
 {
     switch (itemId) {
     //
-    case Icons::add     : button->onClick = [this]() { owner_->addPaths(); };               break;
-    case Icons::remove  : button->onClick = [this]() { owner_->removeSelectedPaths(); };    break;
-    default             : break;
+    case Icons::add         : button->onClick = [this]() { owner_->addPaths(); };                       break;
+    case Icons::remove      : button->onClick = [this]() { owner_->removeSelectedPaths(); };            break;
+    case Icons::synchronize : button->onClick = []() { Spaghettis()->handle (Inputs::rescan (true)); }; break;
+    default                 : break;
     //
     }
 }
