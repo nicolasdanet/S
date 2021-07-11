bca320a9dafedf05c52cd78e506495eb75bcd123
nicolasdanet <nicolas.danet@free.fr>
2021-07-11 10:42:22 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Application/Preferences.cpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Application/Preferences.cpp
@@ -12,13 +12,47 @@ namespace spaghettis {
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
+namespace {
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+void buildConcertinaPanelParameters (const juce::ValueTree& p, juce::Array<juce::PropertyComponent*>& t)
+{
+    juce::String text = p.getProperty (Ids::text).toString();
+    juce::String type = p.getProperty (Ids::type).toString();
+    
+    if (type == "boolean") { t.add (new juce::BooleanPropertyComponent (juce::Value (true), text, "")); }
+    else {
+        t.add (new juce::TextPropertyComponent (juce::Value (juce::var ("Toto")), text, 200, false));
+    }
+}
+
+void buildConcertinaPanelGroup (const juce::ValueTree& group, juce::PropertyPanel& panel)
+{
+    juce::Array<juce::PropertyComponent*> t;
+    
+    for (const auto& p : group) { buildConcertinaPanelParameters (p, t); }
+    
+    panel.addProperties (t);
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
 void Preferences::buildConcertinaPanel (PreferencesComponent& c)
 {
-    for (const auto& s : tree_) {
+    for (const auto& group : tree_) {
     //
-    auto panel = std::make_unique<juce::PropertyPanel> (s.getProperty (Ids::name).toString());
+    auto panel = std::make_unique<juce::PropertyPanel> (group.getProperty (Ids::name).toString());
     
-    //for (const auto& p : s) { }
+    buildConcertinaPanelGroup (group, *panel);
     
     c.addPanel (panel.release());
     //
@@ -36,14 +70,14 @@ juce::ValueTree Preferences::getDefault()
     { Ids::GROUP, {{ Ids::name, "Editing" }},
         {
             { Ids::PARAMETER, {
-                { Ids::item,  "snapToGrid" },
-                { Ids::text,  NEEDS_TRANS ("Snap to grid") },
+                { Ids::item,  "SnapToGrid" },
+                { Ids::text,  NEEDS_TRANS ("Snap to Grid") },
                 { Ids::type,  "boolean" },
                 { Ids::value, true }
             }},
             { Ids::PARAMETER, {
-                { Ids::item, "gridSize" },
-                { Ids::text,  NEEDS_TRANS ("Grid size") },
+                { Ids::item, "GridSize" },
+                { Ids::text,  NEEDS_TRANS ("Grid Size") },
                 { Ids::type, "integer" },
                 { Ids::value, 12 }
             }}
@@ -52,8 +86,8 @@ juce::ValueTree Preferences::getDefault()
     { Ids::GROUP, {{ Ids::name, "Fonts" }},
         {
             { Ids::PARAMETER, {
-                { Ids::item, "defaultFontSize" },
-                { Ids::text,  NEEDS_TRANS ("Default font size") },
+                { Ids::item, "DefaultFontSize" },
+                { Ids::text,  NEEDS_TRANS ("Default Font Size") },
                 { Ids::type, "integer" },
                 { Ids::value, 12 }
             }}
