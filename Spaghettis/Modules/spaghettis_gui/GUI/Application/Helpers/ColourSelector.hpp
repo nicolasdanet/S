331e48203dd1e84657645297dd79f598e41bbcb4
nicolasdanet <nicolas.danet@free.fr>
2021-08-22 14:23:41 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Application/Helpers/ColourSelector.hpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Application/Helpers/ColourSelector.hpp
@@ -134,8 +134,16 @@ public:
 
     void mouseDrag (const juce::MouseEvent& e) override
     {
-        // auto sat =        (float) (e.x - edge_) / (float) (getWidth()  - edge_ * 2);
-        // auto val = 1.0f - (float) (e.y - edge_) / (float) (getHeight() - edge_ * 2);
+        const float x = static_cast<float> (e.x - edge_);
+        const float y = static_cast<float> (e.y - edge_);
+        const float w = static_cast<float> (getWidth() - (edge_ * 2));
+        const float h = static_cast<float> (getHeight() - (edge_ * 2));
+        
+        const float saturation  = x / w;
+        const float value       = 1.0f - (y / h);
+        
+        DBG (juce::String (saturation) + " / " + juce::String (value));
+        
         // owner.setSV (sat, val);
     }
 
