74b0cfe049e58464310270b75ff342bf9ae93b14
nicolasdanet <nicolas.danet@free.fr>
2021-09-07 08:46:03 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Application/Preferences.cpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Application/Preferences.cpp
@@ -154,56 +154,8 @@ juce::ValueTree Preferences::getDefault()
             } }
         }
     },
-    { Ids::GROUP, { { Ids::name, "Nuclear" } },
-        {
-            { Ids::PARAMETER, {
-                { Ids::item,    "Engine" },
-                { Ids::text,    NEEDS_TRANS ("Kind Of Engine") },
-                { Ids::info,    NEEDS_TRANS ("Set kind of stuff") },
-                { Ids::type,    "text" },
-                { Ids::value,   "Submarine" }
-            } },
-            { Ids::PARAMETER, {
-                { Ids::item,    "Power" },
-                { Ids::text,    NEEDS_TRANS ("Power Of Engine") },
-                { Ids::info,    NEEDS_TRANS ("Set power of engine") },
-                { Ids::type,    "float" },
-                { Ids::value,   "99.5" }
-            } },
-            { Ids::PARAMETER, {
-                { Ids::item,    "Launcher" },
-                { Ids::text,    NEEDS_TRANS ("Launcher Efficiency") },
-                { Ids::info,    NEEDS_TRANS ("Set tenderness of button") },
-                { Ids::type,    "float" },
-                { Ids::value,   "99.5" }
-            } },
-            { Ids::PARAMETER, {
-                { Ids::item,    "Random" },
-                { Ids::text,    NEEDS_TRANS ("Random Rate") },
-                { Ids::info,    NEEDS_TRANS ("Set entropy in life") },
-                { Ids::type,    "float" },
-                { Ids::minimum, 0 },
-                { Ids::maximum, 1 },
-                { Ids::value,   "0.5" }
-            } }
-        }
-    },
     { Ids::GROUP, { { Ids::name, "Colors" } },
         {
-            { Ids::PARAMETER, {
-                { Ids::item,    "Bar" },
-                { Ids::text,    NEEDS_TRANS ("Bar") },
-                { Ids::info,    NEEDS_TRANS ("Set bar color") },
-                { Ids::type,    "color" },
-                { Ids::value,   "ffffffff" }
-            } },
-            { Ids::PARAMETER, {
-                { Ids::item,    "Foo" },
-                { Ids::text,    NEEDS_TRANS ("Foo") },
-                { Ids::info,     NEEDS_TRANS ("Set foo color") },
-                { Ids::type,    "color" },
-                { Ids::value,   "ffffffff" }
-            } }
         }
     }
     //
