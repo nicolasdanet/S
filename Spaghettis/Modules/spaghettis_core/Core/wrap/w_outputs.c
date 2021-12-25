326759aecf68a0a044fe206036d0fd38f4c5cda4
nicolasdanet <nicolas.danet@free.fr>
2021-12-25 15:11:28 +0100

    ...

--- Spaghettis/Modules/spaghettis_core/Core/wrap/w_outputs.c
+++ Spaghettis/Modules/spaghettis_core/Core/wrap/w_outputs.c
@@ -122,7 +122,10 @@ PD_LOCAL void outputs_reportCurrentMidiDevices (t_deviceslist *l)
 
 PD_LOCAL void outputs_objectAdded (t_object *x, t_glist *owner)
 {
-    wrapper_send (Outputs::objectAdded (Unique (x, owner), Description::view (x)));
+    const Unique u (x, owner);
+    const Description v (Description::view (x));
+    
+    wrapper_send (Outputs::objectAdded (std::move (u), std::move (v)));
 }
 
 PD_LOCAL void outputs_objectRemoved (t_object *x, t_glist *owner)
