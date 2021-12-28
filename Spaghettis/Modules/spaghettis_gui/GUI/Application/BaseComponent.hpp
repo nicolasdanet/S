2b249aec4a19c6bd2b2c48b3db1146af2287e933
nicolasdanet <nicolas.danet@free.fr>
2021-12-28 07:40:58 +0100

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Application/BaseComponent.hpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Application/BaseComponent.hpp
@@ -85,75 +85,12 @@ public:
 
 public:
     bool getButtonState (int itemId);
+    void loadToolbarButtonsStates();
 
 private:
     void setButtonState (int itemId, bool shouldBeOn);
+    void saveToolbarButtonsStates();
 
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-public:
-    void saveToolbarButtonsStates()
-    {
-        if (toolbar_ && keyName_.isNotEmpty()) {
-        //
-        juce::PropertiesFile& p = Spaghettis()->getProperties();
-        
-        auto root   = std::make_unique<juce::XmlElement> (Ids::BUTTONS);
-        const int n = toolbar_->getNumItems();
-        bool save   = false;
-        
-        for (int i = 0; i < n; ++i) {
-        //
-        IconsButton* b = dynamic_cast<IconsButton*> (toolbar_->getItemComponent (i));
-        if (b && b->isToggle()) {
-            juce::XmlElement* e = root->createNewChildElement (Ids::BUTTON);
-            e->setAttribute (Ids::item,  b->getName());
-            e->setAttribute (Ids::state, b->getState());
-            save = true;
-        }
-        //
-        }
-        
-        if (save) { p.setValue (keyName_ + "Buttons", root.get()); }
-        //
-        }
-    }
-    
-    void loadToolbarButtonsStates()
-    {
-        if (toolbar_ && keyName_.isNotEmpty()) {
-        //
-        juce::PropertiesFile& p = Spaghettis()->getProperties();
-        
-        const std::unique_ptr<juce::XmlElement> root (p.getXmlValue (keyName_ + "Buttons"));
-        
-        if (root && root->hasTagName (Ids::BUTTONS)) {
-
-            for (auto* e : root->getChildWithTagNameIterator (Ids::BUTTON)) {
-                if (e->hasAttribute (Ids::item) && e->hasAttribute (Ids::state)) {
-                    const int itemId = Icons::getInstance().getItemId (e->getStringAttribute (Ids::item));
-                    const bool state = e->getBoolAttribute (Ids::state);
-                    setButtonState (itemId, state);
-                }
-            }
-
-        } else {    /* Default states. */
-        
-            const int n = toolbar_->getNumItems();
-
-            for (int i = 0; i < n; ++i) {
-                IconsButton* b = dynamic_cast<IconsButton*> (toolbar_->getItemComponent (i));
-                if (b && b->isToggle()) {
-                    b->setState (b->getDefaultState());
-                }
-            }
-        }
-        //
-        }
-    }
-    
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
