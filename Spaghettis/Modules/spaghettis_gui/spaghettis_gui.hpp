a3c98ace0fdb6eab43f99729ba23537f7e874e94
nicolasdanet <nicolas.danet@free.fr>
2022-05-14 09:50:33 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/spaghettis_gui.hpp
+++ Spaghettis/Modules/spaghettis_gui/spaghettis_gui.hpp
@@ -124,6 +124,7 @@
 #include "GUI/Patches/PatchWindow.hpp"
 
 #include "GUI/Patches/Graphics/Scalable.hpp"
+#include "GUI/Patches/Graphics/Table.hpp"
 #include "GUI/Patches/Graphics/PainterPolicy.hpp"
 
 #include "GUI/Patches/Graphics/Painters/BangPainter.hpp"
