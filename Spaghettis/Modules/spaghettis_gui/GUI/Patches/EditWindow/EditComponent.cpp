f410ac37e821357067c2d84708873ad92cd6a261
nicolasdanet <nicolas.danet@free.fr>
2022-05-27 08:34:11 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Patches/EditWindow/EditComponent.cpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Patches/EditWindow/EditComponent.cpp
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2021 Jojo and others. */
+/* Copyright (c) 2022 Jojo and others. */
 
 /* < https://opensource.org/licenses/BSD-3-Clause > */
 
@@ -12,74 +12,80 @@ namespace spaghettis {
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-class EditComponent :   protected EditFactoryHelper,    /* MUST be the first. */
-                        public    BaseComponent {
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-public:
-    explicit EditComponent (Patch&, const juce::ValueTree&);
+EditComponent::EditComponent (Patch& owner, const juce::ValueTree& tree) :
+    EditFactoryHelper (this),
+    BaseComponent (getIconsFactory()),
+    owner_ (owner),
+    editView_ (tree),
+    editPort_ (editView_)
+{
+    addAndMakeVisible (editPort_);
+    addChildComponent (zoomComponent_);
     
-    ~EditComponent() = default;
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-public:
-    void paint (juce::Graphics&) override;
-    void resized() override;
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-public:
-    void zoomIn();
-    void zoomOut();
-    void zoomReset();
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-private:
-    void updateZoomComponent();
+    addMenuBarCommand (Commands::zoomIn,    [this]() { zoomIn();    } );
+    addMenuBarCommand (Commands::zoomOut,   [this]() { zoomOut();   } );
+    addMenuBarCommand (Commands::zoomReset, [this]() { zoomReset(); } );
+    
+    setOpaque (true); setSize (600, 300);
+}
     
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-public:
-    bool tryGrabFocus() override
-    {
-        return tryGrabFocusForComponent (this);
+void EditComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (Spaghettis()->getColour (Colours::windowBackground));
+}
+
+void EditComponent::resized()
+{
+    juce::Rectangle<int> bounds (setBoundsForBarsAndGetRemaining());
+    
+    editPort_.setBounds (bounds);
+    
+    updateZoomComponent();
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+void EditComponent::zoomIn()
+{
+    editPort_.zoomIn();
+}
+
+void EditComponent::zoomOut()
+{
+    editPort_.zoomOut();
+}
+
+void EditComponent::zoomReset()
+{
+    editPort_.zoomReset();
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+void EditComponent::updateZoomComponent()
+{
+    const int n = editPort_.getZoom();
+    
+    juce::Rectangle<int> b (getBoundsForToolbar());
+    
+    if (b.getWidth() < 300) { zoomComponent_.setVisible (false); }
+    else {
+    //
+    zoomComponent_.setValue (n);
+    zoomComponent_.setBounds (b.removeFromRight (80).reduced (10, 0));
+    zoomComponent_.setVisible (true);
+    //
     }
+}
     
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-public:
-    Patch& getPatch() const
-    {
-        return owner_;
-    }
-    
-private:
-    Patch& owner_;
-    
-private:
-    EditView editView_;
-    EditPort editPort_;
-    ZoomComponent zoomComponent_;
-    
-private:
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EditComponent)
-};
-
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
@@ -87,4 +93,3 @@ private:
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
-
