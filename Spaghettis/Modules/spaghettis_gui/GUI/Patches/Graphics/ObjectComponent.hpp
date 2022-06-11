e66d6abc209f9d1974afbbf05fe94eebc2e0fa33
nicolasdanet <nicolas.danet@free.fr>
2022-06-11 08:50:54 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/GUI/Patches/Graphics/ObjectComponent.hpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Patches/Graphics/ObjectComponent.hpp
@@ -84,6 +84,7 @@ private:
     std::unique_ptr<PainterPolicy> painter_;
     std::vector<std::unique_ptr<PinComponent>> iPins_;
     std::vector<std::unique_ptr<PinComponent>> oPins_;
+    bool isRunView_;
     
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ObjectComponent)
