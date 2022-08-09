5e4c3f89e95e00ef4fa6d8d481728d12362d63d8
nicolasdanet <nicolas.danet@free.fr>
2022-08-09 10:10:52 +0200

    ...

--- Spaghettis/Modules/spaghettis_gui/spaghettis_gui.cpp
+++ Spaghettis/Modules/spaghettis_gui/spaghettis_gui.cpp
@@ -74,6 +74,7 @@
 
 #include "GUI/Patches/EditWindow/EditFactory.cpp"
 #include "GUI/Patches/EditWindow/EditView.cpp"
+#include "GUI/Patches/EditWindow/EditMouse.cpp"
 #include "GUI/Patches/EditWindow/EditPort.cpp"
 #include "GUI/Patches/EditWindow/EditInspector.cpp"
 #include "GUI/Patches/EditWindow/EditComponent.cpp"
