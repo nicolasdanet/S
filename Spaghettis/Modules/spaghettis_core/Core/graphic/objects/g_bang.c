16a9e96e971881ca3b2b43186551621496685ea6
nicolasdanet <nicolas.danet@free.fr>
2022-12-13 08:13:03 +0100

    ...

--- Spaghettis/Modules/spaghettis_core/Core/graphic/objects/g_bang.c
+++ Spaghettis/Modules/spaghettis_core/Core/graphic/objects/g_bang.c
@@ -89,31 +89,39 @@ static void bng_anything (t_bng *x, t_symbol *s, int argc, t_atom *argv)
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-static void bng_update (t_bng *x, int *t, int n)
+static int bng_update (t_bng *x, int *t, int n)
 {
-    if (n != *t) {
-    //
-    *t = n;
-    #if defined ( PD_BUILDING_APPLICATION )
-    outputs_objectUpdated (cast_object (x), x->x_owner, Tags::parameters());
-    #endif
-    //
+    if (n != *t) { *t = n; return 1; }
+    else {
+        return 0;
     }
 }
 
 static void bng_updateFlashed (t_bng *x, int n)
 {
-    bng_update (x, &x->x_flashed, (n != 0));
+    if (bng_update (x, &x->x_flashed, (n != 0))) {
+        #if defined ( PD_BUILDING_APPLICATION )
+        outputs_objectUpdated (cast_object (x), x->x_owner, Tags::parameters ( { Tag::Flashed } ));
+        #endif
+    }
 }
 
 static void bng_updateFlashTime (t_bng *x, int n)
 {
-    bng_update (x, &x->x_time, PD_CLAMP (n, BANG_TIME_MINIMUM, BANG_TIME_MAXIMUM));
+    if (bng_update (x, &x->x_time, PD_CLAMP (n, BANG_TIME_MINIMUM, BANG_TIME_MAXIMUM))) {
+        #if defined ( PD_BUILDING_APPLICATION )
+        outputs_objectUpdated (cast_object (x), x->x_owner, Tags::parameters ( { Tag::FlashTime } ));
+        #endif
+    }
 }
 
 static void bng_updateWidth (t_bng *x, int n)
 {
-    bng_update (x, &x->x_width, PD_CLAMP (n, BANG_SIZE_MINIMUM, BANG_SIZE_MAXIMUM));
+    if (bng_update (x, &x->x_width, PD_CLAMP (n, BANG_SIZE_MINIMUM, BANG_SIZE_MAXIMUM))) {
+        #if defined ( PD_BUILDING_APPLICATION )
+        outputs_objectUpdated (cast_object (x), x->x_owner, Tags::parameters ( { Tag::Width } ));
+        #endif
+    }
 }
 
 // -----------------------------------------------------------------------------------------------------------
