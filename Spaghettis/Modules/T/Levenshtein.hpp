b37ccbb02372b0564596bca82d86594ef1866cf9
nicolasdanet <nicolas.danet@free.fr>
2023-02-11 08:10:18 +0100

    ...

--- Spaghettis/Modules/T/Levenshtein.hpp
+++ Spaghettis/Modules/T/Levenshtein.hpp
@@ -36,22 +36,22 @@ public:
 // -----------------------------------------------------------------------------------------------------------
 
 public:
-    size_t distance (const T& s1, const T& s2) {
-        const size_t m = s1.size();
-        const size_t n = s2.size();
+    int distance (const T& s1, const T& s2) {
+        const int m = s1.size();
+        const int n = s2.size();
         if (m == 0)
             return n;
         if (n == 0)
             return m;
-        std::vector<size_t> costs(n + 1);
+        std::vector<int> costs(n + 1);
         std::iota(costs.begin(), costs.end(), 0);
-        size_t i = 0;
+        int i = 0;
         for (auto c1 : s1) {
             costs[0] = i + 1;
-            size_t corner = i;
-            size_t j = 0;
+            int corner = i;
+            int j = 0;
             for (auto c2 : s2) {
-                size_t upper = costs[j + 1];
+                int upper = costs[j + 1];
                 costs[j + 1] = (c1 == c2) ? corner
                     : 1 + std::min(std::min(upper, corner), costs[j]);
                 corner = upper;
