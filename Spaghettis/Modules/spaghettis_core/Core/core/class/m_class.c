484bfcb6fae9dde23ed58fa9e686ac05e1f99b97
nicolasdanet <nicolas.danet@free.fr>
2023-02-14 14:42:37 +0100

    ...

--- Spaghettis/Modules/spaghettis_core/Core/core/class/m_class.c
+++ Spaghettis/Modules/spaghettis_core/Core/core/class/m_class.c
@@ -215,7 +215,7 @@ PD_LOCAL t_class *class_new (t_symbol *s,
     
     if (hasSignal) { class_addMethod (c, (t_method)class_setSignals, sym__signals, A_GIMME, A_NULL); }
     
-    if (class_isBox (c)) { class_notify (class_getName (c)); }
+    if (newMethod && class_isBox (c)) { class_notify (class_getName (c)); }
     
     return c;
 }
