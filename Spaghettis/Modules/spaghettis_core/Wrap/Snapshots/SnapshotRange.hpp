9eca892f8f4fe2d179b2b8b7c228ac09e6b4fdff
nicolasdanet <nicolas.danet@free.fr>
2024-01-13 09:31:01 +0100

    ...

--- Spaghettis/Modules/spaghettis_core/Wrap/Snapshots/SnapshotRange.hpp
+++ Spaghettis/Modules/spaghettis_core/Wrap/Snapshots/SnapshotRange.hpp
@@ -12,54 +12,93 @@ namespace spaghettis {
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-class Snapshot {
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-
-friend class Snapshots;
+class SnapshotRange {
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
 public:
-    explicit Snapshot (juce::Range<int>, juce::Range<double>, juce::Rectangle<int>);
+    SnapshotRange() : low_ (0.0), high_ (0.0), width_ (0)
+    {
+    }
     
-    ~Snapshot() = default;
+    ~SnapshotRange() = default;
 
 public:
-    Snapshot (const Snapshot&) = default;
-    Snapshot (Snapshot&&) = default;
-    Snapshot& operator = (const Snapshot&) = default;
-    Snapshot& operator = (Snapshot&&) = default;
+    SnapshotRange (const SnapshotRange&) = default;
+    SnapshotRange (SnapshotRange&&) = default;
+    SnapshotRange& operator = (const SnapshotRange&) = default;
+    SnapshotRange& operator = (SnapshotRange&&) = default;
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-private:
-    void paintCompute();
-    void paintCollapse();
-    void paintProceed (juce::Graphics&);
-    
 public:
-    void paint (juce::Graphics&);
+    void set (double f)
+    {
+        low_    = juce::jmin (f, low_);
+        high_   = juce::jmax (f, high_);
+        width_  = 1;
+    }
+
+    void enlarge()
+    {
+        ++width_;
+    }
     
-private:
-    void fetch (void*, int);
+    void scale (juce::Range<double> range, juce::Rectangle<int> painted)
+    {
+        const double offset        = range.getEnd();
+        const double valuePerPixel = range.getLength() / painted.getHeight();
+        
+        const double a = (offset - high_) / valuePerPixel;
+        const double b = (offset - low_)  / valuePerPixel;
+        
+        r_ = juce::Range<int> (static_cast<int> (std::round (a)), static_cast<int> (std::round (b)));
+    }
     
-private:
-    juce::Range<int> domain_;
-    juce::Range<double> range_;
-    juce::Rectangle<int> painted_;
-    std::vector<SnapshotRange> v_;
+    bool collapse (SnapshotRange& s)
+    {
+        if (r_ == s.r_) { width_ += s.width_; s.width_ = 0; return true; }
+        
+        return false;
+    }
     
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+public:
+    juce::Rectangle<float> getRectangle() const
+    {
+        juce::Point<float> a = juce::Point<float> (0, r_.getStart());
+        juce::Point<float> b = juce::Point<float> (getWidth(), r_.getEnd());
+    
+        if (high_ == 0.0) { a = a.translated (0.0, -0.5f); }
+        if (low_  == 0.0) { b = b.translated (0.0,  0.5f); }
+        
+        return juce::Rectangle<float> (a, b);
+    }
+
+    int getWidth() const
+    {
+        return width_;
+    }
+    
+    bool isSet() const
+    {
+        return (width_ > 0);
+    }
+    
+private:
+    double low_;
+    double high_;
+    int width_;
 
 private:
-    JUCE_LEAK_DETECTOR (Snapshot)
+    juce::Range<int> r_;
 };
 
 // -----------------------------------------------------------------------------------------------------------
