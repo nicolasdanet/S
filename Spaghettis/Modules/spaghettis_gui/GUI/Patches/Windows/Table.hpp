c303e1f8cd89852e7477417950d7fa2d56a88d4d
nicolasdanet <nicolas.danet@free.fr>
2024-02-06 07:35:45 +0100

    ... checker

--- Spaghettis/Modules/spaghettis_gui/GUI/Patches/Windows/Table.hpp
+++ Spaghettis/Modules/spaghettis_gui/GUI/Patches/Windows/Table.hpp
@@ -12,6 +12,21 @@ namespace spaghettis {
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
+#if JUCE_DEBUG
+
+#define CHECK_REENTRANCY    jassert (reentrant_ == false); \
+                            const juce::ScopedValueSetter<bool> setter (reentrant_, true, false);
+                                
+#else
+    
+#define CHECK_REENTRANCY
+    
+#endif
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
 template <class T> class Table {
 
 // -----------------------------------------------------------------------------------------------------------
@@ -28,16 +43,22 @@ public:
 public:
     template <class U> void add (PatchView* owner, const U& t)
     {
+        CHECK_REENTRANCY
+        
         v_.push_back (std::make_unique<T> (owner, t));
     }
     
     void remove (core::UniqueId u)
     {
+        CHECK_REENTRANCY
+        
         v_.erase (std::remove_if (v_.begin(), v_.end(), hasSameIdentifier (u)), v_.end());
     }
     
     void sort (const std::vector<core::UniqueId>& t)
     {
+        CHECK_REENTRANCY
+        
         jassert (v_.size() == t.size());
         
         int i = 0; for (auto u : t) { moveAt (i++, u); }
@@ -45,6 +66,8 @@ public:
     
     void clear()
     {
+        CHECK_REENTRANCY
+        
         v_.clear();
     }
 
@@ -67,12 +90,25 @@ public:
 public:
     template <class F> void doForEach (F f) const
     {
+        CHECK_REENTRANCY
+        
         for_each (v_.cbegin(), v_.cend(), f);
     }
     
     template <class F> void doForEachReversed (F f) const
     {
+        CHECK_REENTRANCY
+        
         for_each (v_.crbegin(), v_.crend(), f);
+    }
+    
+    template <class F> void doForUnique (F f, core::UniqueId identifier) const
+    {
+        CHECK_REENTRANCY
+        
+        auto r = std::find_if (v_.cbegin(), v_.cend(), hasSameIdentifier (identifier));
+        
+        if (r != v_.cend()) { f (*r); }
     }
     
     template <class F> void doForEachSelected (F f) const
@@ -80,13 +116,6 @@ public:
         auto g = [h = f](const auto& p) { if (p->isSelected()) { h (p); } };
         
         doForEach (g);
-    }
-
-    template <class F> void doForUnique (F f, core::UniqueId identifier) const
-    {
-        auto r = std::find_if (v_.cbegin(), v_.cend(), hasSameIdentifier (identifier));
-        
-        if (r != v_.cend()) { f (*r); }
     }
     
     template <class F> auto doCountIf (F f) const
@@ -124,10 +153,22 @@ private:
     
 private:
     std::vector<std::unique_ptr<T>> v_;
+
+#if JUCE_DEBUG
+
+private:
+    mutable bool reentrant_ { false };
+    
+#endif
     
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Table)
 };
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+#undef CHECK_REENTRANCY
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
