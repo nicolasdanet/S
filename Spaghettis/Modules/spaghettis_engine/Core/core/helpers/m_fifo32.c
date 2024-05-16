424a59c510d4b1571a58c4dfe80528de910228e4
nicolasdanet <nicolas.danet@free.fr>
2024-05-16 08:52:07 +0200

    ...

--- Spaghettis/Modules/spaghettis_engine/Core/core/helpers/m_fifo32.c
+++ Spaghettis/Modules/spaghettis_engine/Core/core/helpers/m_fifo32.c
@@ -12,9 +12,8 @@
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-#define FIFO32_SIZE     (8192)                  /* Must be a power of two. */
-#define FIFO32_MASK     (FIFO32_SIZE - 1)
-#define FIFO32_BYTES    (4)
+#define FIFO32_SIZE     8192    /* Must be a power of two. */
+#define FIFO32_BYTES    4
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
@@ -48,78 +47,14 @@ int fifo32_getAvailableWrite (t_fifo32 *x)
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-/* https://en.cppreference.com/w/cpp/language/reinterpret_cast#Type_aliasing */
-
-static void fifo32_writeProceed (t_fifo32 *x, const void *data, uint64_t index, int writted)
-{
-    #if defined ( __cplusplus )
-    const char *p = static_cast<const char*> (data);
-    #else
-    const char *p = data;
-    #endif
-    
-    while (writted--) {
-    //
-    int t = (index & FIFO32_MASK) * FIFO32_BYTES;
-    
-    memcpy (x->f_vector + t, p, FIFO32_BYTES);
-    
-    p     += FIFO32_BYTES;
-    index += 1;
-    //
-    }
-}
-
 int fifo32_write (t_fifo32 *x, const void *data, int n)
 {
-    int available  = fifo32_getAvailableWrite (x);
-    int writted    = PD_CLAMP (n, 0, available);
-    
-    uint64_t index = atomic_uInt64ReadRelaxed (&x->f_write);
-    
-    fifo32_writeProceed (x, data, index, writted);
-    
-    atomic_uInt64WriteRelease (&x->f_write, index + writted);
-    
-    return writted;
-}
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-static void fifo32_readProceed (t_fifo32 *x, void *data, uint64_t index, int readed)
-{
-    #if defined ( __cplusplus )
-    char *p = static_cast<char*> (data);
-    #else
-    char *p = data;
-    #endif
-    
-    while (readed--) {
-    //
-    int t = (index & FIFO32_MASK) * FIFO32_BYTES;
-    
-    memcpy (p, x->f_vector + t, FIFO32_BYTES);
-    
-    p     += FIFO32_BYTES;
-    index += 1;
-    //
-    }
+    return fifo_shared_write (x, FIFO32_SIZE, FIFO32_BYTES, data, n);
 }
 
 int fifo32_read (t_fifo32 *x, void *data, int n)
 {
-    int available  = fifo32_getAvailableRead (x);
-    int readed     = PD_CLAMP (n, 0, available);
-    
-    uint64_t index = atomic_uInt64ReadRelaxed (&x->f_read);
-    
-    fifo32_readProceed (x, data, index, readed);
-    
-    atomic_uInt64WriteRelease (&x->f_read, index + readed);
-    
-    return readed;
+    return fifo_shared_read (x, FIFO32_SIZE, FIFO32_BYTES, data, n);
 }
 
 // -----------------------------------------------------------------------------------------------------------
--- Spaghettis/Modules/spaghettis_engine/Core/core/helpers/m_fifo8.c
+++ Spaghettis/Modules/spaghettis_engine/Core/core/helpers/m_fifo8.c
@@ -12,9 +12,8 @@
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-#define FIFO8_SIZE  (131072)                /* Must be a power of two. */
-#define FIFO8_MASK  (FIFO8_SIZE - 1)
-#define FIFO8_BYTES (1)
+#define FIFO8_SIZE      131072      /* Must be a power of two. */
+#define FIFO8_BYTES     1
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
@@ -48,78 +47,14 @@ int fifo8_getAvailableWrite (t_fifo8 *x)
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-/* https://en.cppreference.com/w/cpp/language/reinterpret_cast#Type_aliasing */
-
-static void fifo8_writeProceed (t_fifo8 *x, const void *data, uint64_t index, int writted)
-{
-    #if defined ( __cplusplus )
-    const char *p = static_cast<const char*> (data);
-    #else
-    const char *p = data;
-    #endif
-    
-    while (writted--) {
-    //
-    int t = (index & FIFO8_MASK) * FIFO8_BYTES;
-    
-    memcpy (x->f_vector + t, p, FIFO8_BYTES);
-    
-    p     += FIFO8_BYTES;
-    index += 1;
-    //
-    }
-}
-
 int fifo8_write (t_fifo8 *x, const void *data, int n)
 {
-    int available  = fifo8_getAvailableWrite (x);
-    int writted    = PD_CLAMP (n, 0, available);
-    
-    uint64_t index = atomic_uInt64ReadRelaxed (&x->f_write);
-    
-    fifo8_writeProceed (x, data, index, writted);
-    
-    atomic_uInt64WriteRelease (&x->f_write, index + writted);
-    
-    return writted;
-}
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-static void fifo8_readProceed (t_fifo8 *x, void *data, uint64_t index, int readed)
-{
-    #if defined ( __cplusplus )
-    char *p = static_cast<char*> (data);
-    #else
-    char *p = data;
-    #endif
-    
-    while (readed--) {
-    //
-    int t = (index & FIFO8_MASK) * FIFO8_BYTES;
-    
-    memcpy (p, x->f_vector + t, FIFO8_BYTES);
-    
-    p     += FIFO8_BYTES;
-    index += 1;
-    //
-    }
+    return fifo_shared_write (x, FIFO8_SIZE, FIFO8_BYTES, data, n);
 }
 
 int fifo8_read (t_fifo8 *x, void *data, int n)
 {
-    int available  = fifo8_getAvailableRead (x);
-    int readed     = PD_CLAMP (n, 0, available);
-    
-    uint64_t index = atomic_uInt64ReadRelaxed (&x->f_read);
-    
-    fifo8_readProceed (x, data, index, readed);
-    
-    atomic_uInt64WriteRelease (&x->f_read, index + readed);
-    
-    return readed;
+    return fifo_shared_read (x, FIFO8_SIZE, FIFO8_BYTES, data, n);
 }
 
 // -----------------------------------------------------------------------------------------------------------
