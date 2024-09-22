d0fcb8cedec815e28480fe9c22965768d3a240c2
nicolasdanet <nicolas.danet@free.fr>
2024-09-22 08:03:46 +0200

    ...

--- Spaghettis/Modules/spaghettis_core/Glue/Entry.cpp
+++ Spaghettis/Modules/spaghettis_core/Glue/Entry.cpp
@@ -6,52 +6,78 @@
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-#include "spaghettis_core.hpp"
+namespace spaghettis::core {
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-#include "Core/amalgam.cpp"
+extern int  main_argc;
+
+extern char **main_argv;
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-#include "Geometry/Vector.cpp"
-#include "Geometry/Point.cpp"
-#include "Geometry/Area.cpp"
-#include "Geometry/Geometry.cpp"
+Wrapper *main_wrapper;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+void main_threadLoopFakeCommandLine (const juce::StringArray& cmd)
+{
+    int n = cmd.size();
+    
+    if (n) {
+    //
+    main_argc = n;
+    main_argv = static_cast<char**> (malloc (sizeof (char*) * n));
+    
+    for (int i = 0; i < n; ++i) {
+        const juce::String& s = cmd[i];
+        const size_t size = s.getNumBytesAsUTF8() + 1;
+        main_argv[i] = static_cast<char*> (malloc (size));
+        s.copyToUTF8 (main_argv[i], size);
+    }
+    //
+    } else { jassertfalse; }
+}
+
+void main_threadLoopFakeCommandLineRelease()
+{
+    for (int i = 0; i < main_argc; ++i) { free (main_argv[i]); }
+    
+    free (main_argv);
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+bool main_threadLoop (Wrapper *owner)
+{
+    main_wrapper = owner;
+
+    main_threadLoopFakeCommandLine (main_wrapper->getCommandLine());
+    
+    PD_ASSERT (sys_isControlThread());
+    
+    int error = main_start();
+    
+    main_threadLoopFakeCommandLineRelease();
+    
+    return (error != 0);
+}
+
+void main_threadExit()
+{
+    main_exit();
+}
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-#include "Glue/Entry.cpp"
-#include "Glue/Glue.cpp"
-#include "Glue/Maker.cpp"
-#include "Glue/String.cpp"
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-
-#include "Item/Helpers/Unique.cpp"
-#include "Item/Helpers/Tree.cpp"
-#include "Item/Item.cpp"
-#include "Item/Object.cpp"
-#include "Item/Patch.cpp"
-#include "Item/Builder/Report.cpp"
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-
-#include "Documentation/Documentation.cpp"
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-
-#include "Wrap/Devices/Devices.cpp"
-#include "Wrap/Commands/Inputs.cpp"
-#include "Wrap/Snapshots/Snapshot.cpp"
-#include "Wrap/Snapshots/Snapshots.cpp"
-#include "Wrap/Wrapper.cpp"
+}
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
