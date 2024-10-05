e6a71ff21004722ac0aac92050f1cf4876b8927b
nicolasdanet <nicolas.danet@free.fr>
2024-10-05 07:17:27 +0200

    ...

--- Spaghettis/Modules/spaghettis_core/Core/system/log/s_debug.c
+++ Spaghettis/Modules/spaghettis_core/Core/system/log/s_debug.c
@@ -12,6 +12,11 @@
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
+#include <execinfo.h>
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
 #if PD_WITH_DEBUG
     
 void post_syslog (const char *s)
@@ -26,6 +31,21 @@ void post_syslog (const char *s)
 void post_syslogFloat (double f)
 {
     char t[LOGGER_FLOAT_STRING] = { 0 }; post_syslog (logger_stringWithFloat (t, f));
+}
+
+/* https://www.gnu.org/software/libc/manual/html_node/Backtraces.html */
+
+void post_backtrace (void)
+{
+    void *t[128]  = { 0 };
+    int i, size   = backtrace (t, 128);
+    char **frames = backtrace_symbols (t, size);
+    
+    if (frames != NULL) {
+        for (i = 0; i < size; i++) { post_syslog (frames[i]); }
+    }
+
+    free (frames);
 }
 
 #endif
--- Spaghettis/Modules/spaghettis_core/Core/system/log/s_debug.h
+++ Spaghettis/Modules/spaghettis_core/Core/system/log/s_debug.h
@@ -17,11 +17,13 @@
 
 #define PD_DBG(s)           post_syslog (s)
 #define PD_DBG_NUMBER(f)    post_syslogFloat ((double)(f))
+#define PD_DBG_BACKTRACE    post_backtrace()
 
 #else
 
 #define PD_DBG(s)
 #define PD_DBG_NUMBER(f)
+#define PD_DBG_BACKTRACE
 
 #endif
 
@@ -31,6 +33,7 @@
 
 void post_syslog        (const char *s);
 void post_syslogFloat   (double f);
+void post_backtrace     (void);
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
