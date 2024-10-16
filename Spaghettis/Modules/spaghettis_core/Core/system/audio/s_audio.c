af010d906eecf8a564aabedc225be1caa598b4bb
nicolasdanet <nicolas.danet@free.fr>
2024-10-16 15:03:40 +0200

    ...

--- Spaghettis/Modules/spaghettis_core/Core/system/audio/s_audio.c
+++ Spaghettis/Modules/spaghettis_core/Core/system/audio/s_audio.c
@@ -37,9 +37,102 @@ static void audio_vectorInitialize (t_float, int, int);
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-int audio_isOpened (void)
+t_error audio_start (void)
 {
-    return (audio_state != 0);
+    if (!audio_isOpened()) { return audio_open(); } else { return PD_ERROR_NONE; }
+}
+
+t_error audio_stop (void)
+{
+    if (audio_isOpened()) { audio_close(); } return PD_ERROR_NONE;
+}
+
+int audio_poll (void)
+{
+    int k = DACS_NO;
+    
+    if (pthread_mutex_trylock (&audio_mutex) == 0) {
+    //
+    if (audio_isOpened()) { k = audio_pollNative(); }
+    
+    pthread_mutex_unlock (&audio_mutex);
+    //
+    }
+    
+    return k;
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+double audio_getNanosecondsToSleep (void)
+{
+    double t = INTERNAL_BLOCKSIZE / audio_getSampleRate();
+    
+    return PD_SECONDS_TO_NANOSECONDS (t);
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+t_float audio_getSampleRate (void)
+{
+    t_float f = atomic_float64Read (&audio_sampleRate); return (f <= 0.0 ? AUDIO_DEFAULT_SAMPLERATE : f);
+}
+
+int audio_getVectorSize (void)
+{
+    return audio_getVectorSizeNative();
+}
+
+int audio_getTotalOfChannelsIn (void) 
+{
+    return audio_totalOfChannelsIn;
+}
+
+int audio_getTotalOfChannelsOut (void)
+{
+    return audio_totalOfChannelsOut; 
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+static void audio_vectorInitialize (t_float sampleRate, int totalOfChannelsIn, int totalOfChannelsOut)
+{
+    int m = (int)((INTERNAL_BLOCKSIZE * sizeof (t_sample)) * (totalOfChannelsIn ? totalOfChannelsIn : 2));
+    int n = (int)((INTERNAL_BLOCKSIZE * sizeof (t_sample)) * (totalOfChannelsOut ? totalOfChannelsOut : 2));
+
+    PD_ASSERT (totalOfChannelsIn >= 0);
+    PD_ASSERT (totalOfChannelsOut >= 0);
+    
+    if (audio_soundIn)  { garbage_newRaw ((void *)audio_soundIn);  audio_soundIn  = NULL; }
+    if (audio_soundOut) { garbage_newRaw ((void *)audio_soundOut); audio_soundOut = NULL; }
+    
+    audio_soundIn  = (t_sample *)PD_MEMORY_GET (m);
+    audio_soundOut = (t_sample *)PD_MEMORY_GET (n);
+    
+    audio_totalOfChannelsIn  = totalOfChannelsIn;
+    audio_totalOfChannelsOut = totalOfChannelsOut;
+    
+    atomic_float64Write (&audio_sampleRate, sampleRate);
+}
+
+void audio_vectorShrinkIn (int totalOfChannelsIn)
+{
+    PD_ASSERT (totalOfChannelsIn <= audio_totalOfChannelsIn);
+    
+    audio_totalOfChannelsIn = totalOfChannelsIn;
+}
+
+void audio_vectorShrinkOut (int totalOfChannelsOut)
+{
+    PD_ASSERT (totalOfChannelsOut <= audio_totalOfChannelsOut);
+    
+    audio_totalOfChannelsOut = totalOfChannelsOut;
 }
 
 // -----------------------------------------------------------------------------------------------------------
@@ -142,106 +235,13 @@ void audio_close (void)
     pthread_mutex_unlock (&audio_mutex);
 }
 
-int audio_poll (void)
-{
-    int k = DACS_NO;
-    
-    if (pthread_mutex_trylock (&audio_mutex) == 0) {
-    //
-    if (audio_isOpened()) { k = audio_pollNative(); }
-    
-    pthread_mutex_unlock (&audio_mutex);
-    //
-    }
-    
-    return k;
-}
-
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-t_error audio_stop (void)
+int audio_isOpened (void)
 {
-    if (audio_isOpened()) { audio_close(); } return PD_ERROR_NONE;
-}
-
-t_error audio_start (void)
-{
-    if (!audio_isOpened()) { return audio_open(); } else { return PD_ERROR_NONE; }
-}
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-double audio_getNanosecondsToSleep (void)
-{
-    double t = INTERNAL_BLOCKSIZE / audio_getSampleRate();
-    
-    return PD_SECONDS_TO_NANOSECONDS (t);
-}
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-t_float audio_getSampleRate (void)
-{
-    t_float f = atomic_float64Read (&audio_sampleRate); return (f <= 0.0 ? AUDIO_DEFAULT_SAMPLERATE : f);
-}
-
-int audio_getVectorSize (void)
-{
-    return audio_getVectorSizeNative();
-}
-
-int audio_getTotalOfChannelsIn (void) 
-{
-    return audio_totalOfChannelsIn;
-}
-
-int audio_getTotalOfChannelsOut (void)
-{
-    return audio_totalOfChannelsOut; 
-}
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-static void audio_vectorInitialize (t_float sampleRate, int totalOfChannelsIn, int totalOfChannelsOut)
-{
-    int m = (int)((INTERNAL_BLOCKSIZE * sizeof (t_sample)) * (totalOfChannelsIn ? totalOfChannelsIn : 2));
-    int n = (int)((INTERNAL_BLOCKSIZE * sizeof (t_sample)) * (totalOfChannelsOut ? totalOfChannelsOut : 2));
-
-    PD_ASSERT (totalOfChannelsIn >= 0);
-    PD_ASSERT (totalOfChannelsOut >= 0);
-    
-    if (audio_soundIn)  { garbage_newRaw ((void *)audio_soundIn);  audio_soundIn  = NULL; }
-    if (audio_soundOut) { garbage_newRaw ((void *)audio_soundOut); audio_soundOut = NULL; }
-    
-    audio_soundIn  = (t_sample *)PD_MEMORY_GET (m);
-    audio_soundOut = (t_sample *)PD_MEMORY_GET (n);
-    
-    audio_totalOfChannelsIn  = totalOfChannelsIn;
-    audio_totalOfChannelsOut = totalOfChannelsOut;
-    
-    atomic_float64Write (&audio_sampleRate, sampleRate);
-}
-
-void audio_vectorShrinkIn (int totalOfChannelsIn)
-{
-    PD_ASSERT (totalOfChannelsIn <= audio_totalOfChannelsIn);
-    
-    audio_totalOfChannelsIn = totalOfChannelsIn;
-}
-
-void audio_vectorShrinkOut (int totalOfChannelsOut)
-{
-    PD_ASSERT (totalOfChannelsOut <= audio_totalOfChannelsOut);
-    
-    audio_totalOfChannelsOut = totalOfChannelsOut;
+    return (audio_state != 0);
 }
 
 // -----------------------------------------------------------------------------------------------------------
