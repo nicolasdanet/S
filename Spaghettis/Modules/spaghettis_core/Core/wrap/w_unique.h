6efb0f3b1e44da7cda4909462287dfd4be88b2eb
nicolasdanet <nicolas.danet@free.fr>
2024-10-22 14:58:27 +0200

    ...

--- Spaghettis/Modules/spaghettis_core/Core/wrap/w_unique.h
+++ Spaghettis/Modules/spaghettis_core/Core/wrap/w_unique.h
@@ -14,55 +14,31 @@
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-t_error    unique_objectSelect         (t_id u);
-t_error    unique_objectDeselect       (t_id u);
-t_error    unique_objectSnap           (t_id u);
-t_error    unique_objectMoveBack       (t_id u);
-t_error    unique_objectMoveFront      (t_id u);
-t_error    unique_objectRemove         (t_id u);
-t_error    unique_objectOrientation    (t_id u, int isVertical);
-t_error    unique_objectResize         (t_id u, int w, int h);
-t_error    unique_objectPosition       (t_id u, int x, int y);
+t_error    unique_objectSelect                  (t_id u);
+t_error    unique_objectDeselect                (t_id u);
+t_error    unique_objectSnap                    (t_id u);
+t_error    unique_objectMoveBack                (t_id u);
+t_error    unique_objectMoveFront               (t_id u);
+t_error    unique_objectRemove                  (t_id u);
+t_error    unique_objectOrientation             (t_id u, int isVertical);
+t_error    unique_objectResize                  (t_id u, int w, int h);
+t_error    unique_objectPosition                (t_id u, int x, int y);
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-t_error    unique_objectDisplace       (t_id u, int deltaX, int deltaY);
-t_error    unique_objectMoveAtFirst    (t_id u);
-t_error    unique_objectMoveAtLast     (t_id u);
-t_error    unique_objectMoveAt         (t_id u, int n);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-t_error    unique_objectBang           (t_id u);
-t_error    unique_objectFloat          (t_id u, t_float);
-t_error    unique_objectMessage        (t_id u, t_symbol *s, int argc, t_atom *argv);
+t_error    unique_objectDisplace                (t_id u, int deltaX, int deltaY);
+t_error    unique_objectMoveAtFirst             (t_id u);
+t_error    unique_objectMoveAtLast              (t_id u);
+t_error    unique_objectMoveAt                  (t_id u, int n);
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-#if defined ( PD_BUILDING_APPLICATION )
-
-t_error    unique_objectParameter      (t_id u, const data::Group&);
-t_error    unique_objectHelp           (t_id u);
-
-#endif
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-t_error    unique_objectGetIndexOf     (t_id u, int *n);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-t_error    unique_objectLineConnect    (t_id u, int m, t_id v, int n);
-t_error    unique_objectLineDisconnect (t_id u, int m, t_id v, int n);
+t_error    unique_objectBang                    (t_id u);
+t_error    unique_objectFloat                   (t_id u, t_float);
+t_error    unique_objectMessage                 (t_id u, t_symbol *s, int argc, t_atom *argv);
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
@@ -70,7 +46,8 @@ t_error    unique_objectLineDisconnect (t_id u, int m, t_id v, int n);
 
 #if defined ( PD_BUILDING_APPLICATION )
 
-t_error    unique_patchCreateObject    (t_id u, Point::Real, const juce::String&);
+t_error    unique_objectParameter               (t_id u, const data::Group&);
+t_error    unique_objectHelp                    (t_id u);
 
 #endif
 
@@ -78,24 +55,47 @@ t_error    unique_patchCreateObject    (t_id u, Point::Real, const juce::String&
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-t_error    unique_patchClose           (t_id u);
-t_error    unique_patchSave            (t_id u);
-t_error    unique_patchSetDirty        (t_id u);
+t_error    unique_objectGetIndexOf              (t_id u, int *n);
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+t_error    unique_objectLineConnect             (t_id u, int m, t_id v, int n);
+t_error    unique_objectLineDisconnect          (t_id u, int m, t_id v, int n);
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+#if defined ( PD_BUILDING_APPLICATION )
+
+t_error    unique_patchCreateObject             (t_id u, Point::Real, const juce::String&);
+
+#endif
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
+t_error    unique_patchClose                    (t_id u);
+t_error    unique_patchSave                     (t_id u);
+t_error    unique_patchSetDirty                 (t_id u);
 
 t_error    unique_patchSetEditView              (t_id u, t_rectangle *r);
 t_error    unique_patchSetEditViewProperties    (t_id u, int, int, int, int, int);
 
-t_error    unique_patchUndo            (t_id u);
-t_error    unique_patchRedo            (t_id u);
+t_error    unique_patchUndo                     (t_id u);
+t_error    unique_patchRedo                     (t_id u);
 
-t_error    unique_patchCut             (t_id u);
-t_error    unique_patchCopy            (t_id u);
-t_error    unique_patchPaste           (t_id u, t_point *m);
-t_error    unique_patchDuplicate       (t_id u);
-t_error    unique_patchRemove          (t_id u);
+t_error    unique_patchCut                      (t_id u);
+t_error    unique_patchCopy                     (t_id u);
+t_error    unique_patchPaste                    (t_id u, t_point *m);
+t_error    unique_patchDuplicate                (t_id u);
+t_error    unique_patchRemove                   (t_id u);
 
-t_error    unique_patchEncapsulate     (t_id u);
-t_error    unique_patchDeencapsulate   (t_id u);
+t_error    unique_patchEncapsulate              (t_id u);
+t_error    unique_patchDeencapsulate            (t_id u);
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
